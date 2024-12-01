e4c6fe71d4ad3f8bf81eb7c4c0ee92e3da6c1824
ProjectBM <qud@umich.edu>
2024-11-30 21:31:03 -0800

    Complete day 1

--- 01/code.py
+++ 01/code.py
@@ -1,12 +1,40 @@
 from AoCUtils.utils import *
+from collections import defaultdict
 
 
 def part_one():
-    return
+    left_list = []
+    right_list = []
+
+    lines = parse_input("input1.txt")
+    for line in lines:
+        split_line = line.split("   ")
+        left_list.append(int(split_line[0]))
+        right_list.append(int(split_line[1]))
+
+    left_list = sorted(left_list)
+    right_list = sorted(right_list)
+
+    total = 0
+    for i in range(len(left_list)):
+        total += abs(left_list[i] - right_list[i])
+    return total
 
 
 def part_two():
-    return
+    left_list = []
+    right_list = defaultdict(int)
+
+    lines = parse_input("input1.txt")
+    for line in lines:
+        split_line = line.split("   ")
+        left_list.append(int(split_line[0]))
+        right_list[int(split_line[1])] += 1
+
+    total = 0
+    for id in left_list:
+        total += id * right_list[id]
+    return total
 
 
 run_code(part_one, part_two)
